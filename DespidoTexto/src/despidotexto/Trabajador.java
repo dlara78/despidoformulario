e0fe025e958d35fddb52771493be2505a66f2b6c
dlara78 <david.lara78@gmail.com>
2014-08-05 21:05:46 +0200

    Trabando en los primeros ficheros del programa de calculo de antigüedad.

---
+++ DespidoTexto/src/despidotexto/Trabajador.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package despidotexto;
+
+import java.util.GregorianCalendar;
+
+/**
+ *
+ * @author david
+ */
+public class Trabajador {
+   
+    String nombre;
+    String empresa;
+    GregorianCalendar fechaAlta;
+    GregorianCalendar fechaBaja;
+    float baseCotizacion;
+    
+    
+    public Trabajador(String nombre, String empresa, GregorianCalendar fechaAlta, GregorianCalendar fechaBaja, float baseCotizacion){
+        
+        this.nombre = nombre;
+        this.empresa = empresa;
+        this.fechaAlta = fechaAlta;
+        this.fechaBaja = fechaBaja;
+        this.baseCotizacion = baseCotizacion;
+        
+    }
+    
+    public int calcularAntiguedad(GregorianCalendar fechaAlta, GregorianCalendar fechaBaja){
+        
+        
+        return int antiguedadTotal;
+    }
+    
+}
