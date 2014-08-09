cec1f2a422ca594ff2045d40f93c560dee0bcde8
dlara78 <david.lara78@gmail.com>
2014-08-09 14:26:07 +0200

    Trabajo en el cálculo de la diferencia total de dias en el formulario. Borro algunos ficheros .java que no se usan para nada. Importo otros ficheros .java de otro proyecto para usarlos aquí.

--- DespidoTexto/src/DespidoFormulario/Metodos.java
+++ DespidoTexto/src/DespidoFormulario/Metodos.java
@@ -1,16 +1,48 @@
 package DespidoFormulario;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Metodos {
-   
-    float baseDiaria;
-    
-    public float calculaBaseDiaria (float bCotizacion, float numDiasMes){   
-        baseDiaria = bCotizacion/numDiasMes;
-        return baseDiaria;
+
+        float MILISEGS_POR_DIA = (24 * 60 * 60 * 1000);
+
+  /*El siguiente método devuelve el valor en milis (float) de la fecha enviada 
+     * por string
+     */
+    public float calcularFechaEnMilis(String fechaString) {
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+
+        //Fecha de alta
+        Date dateFecha = null;
+        try {
+            dateFecha = df.parse(fechaString);
+        } catch (ParseException ex) {
+            Logger.getLogger(NewJFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
-    
-    public void recordarFormatoFecha(){
-      
+        Calendar calFechaIntroducida = Calendar.getInstance();
+        calFechaIntroducida.setTime(dateFecha);
+        float fechaEnMilis = calFechaIntroducida.getTimeInMillis();
+
+        return fechaEnMilis;
+    }
+
+    /*
+    * Al sgte método se le mandan dos fechas (en String) y devuelve la diferencia en días,
+    * en un solo valor float.
+    */
+    public float calcularDifEntreDosFechas(String fechaBaja, String fechaAlta) {
+
+        float diferenciaEnMilis = this.calcularFechaEnMilis(fechaBaja) - this.calcularFechaEnMilis(fechaAlta);
+        float diferenciaEnDias = diferenciaEnMilis / MILISEGS_POR_DIA;
+
+        return diferenciaEnDias;
     }
     
 }
